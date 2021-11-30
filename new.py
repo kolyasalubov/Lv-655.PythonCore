c4c438574d89e0df951a43cc96b32f88848abea9
Liubov Koliasa <lkoliasa@mail.com>
2021-11-30 20:07:33 +0200

    message

---
+++ new.py
@@ -0,0 +1,41 @@
+# # a = {45:"67", 
+# #      78:"67", 
+# #      89:"777"}
+
+# # print(a)
+
+# # PATH1 = "\path\table\name"
+# # PATH2 = r"\path\table\name"
+
+# # print(PATH1)
+# # print(PATH2)
+
+# # name = "John"
+# # age = 23
+# # salary = 999.99
+# # # print("%s is %d years old. Your sallary is %.3f $" % (name, age, salary))
+
+# # default_order = f"Hello, {salary}, {age} and {name}"
+# # print(default_order)
+# # order using positional argument
+# # positional_order = "{1}, {0} and {2}".format('John','Bill','Sean')
+# # print(positional_order)
+# # # order using keyword argument
+# # keyword_order = "{s}, {b} and {j}".format(j='John',b='Bill',s='Sean')
+# # print(keyword_order)
+
+
+# str = 'programiz'
+# print('str[:] = ', id(str), id(str[:]))
+
+
+t = "PrOgRaMiZ"
+d = t.upper()
+print(id(t), id(d))
+
+
+
+
+
+
+
