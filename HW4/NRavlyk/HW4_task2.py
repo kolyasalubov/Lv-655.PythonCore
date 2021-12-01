12c644b7676bc1b317ff524d6ba158594710f004
Nazar Ravlyk <ravlyk.n@gmail.com>
2021-12-01 23:02:40 +0200

    corrected the error

--- HW4/NRavlyk/HW4_task2.py
+++ HW4/NRavlyk/HW4_task2.py
@@ -8,10 +8,10 @@ while True:
     except:
         print("Once more")
 
-listofnumber = list(range(number))
+listofnumber = list(range(0,number,2))
 
 print(listofnumber)
 
-for x in listofnumber:
+for x in range(0,len(listofnumber)):
     listofnumber[x] = float(listofnumber[x])
 print(listofnumber)
