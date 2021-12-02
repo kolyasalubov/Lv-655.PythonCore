f7d36460cbf46918ed67c5b9bb499a26ab4efe1a
DykyiVolodymyr <qwertyd987@gmail.com>
2021-12-02 13:52:42 +0200

    HW1

---
+++ HW1/vDykyi/task2.py
@@ -0,0 +1,8 @@
+chyslo1 = 23.7
+chyslo2 = 40
+
+print(chyslo1 - chyslo2)
+print(chyslo1 % 3)
+print(chyslo1 * chyslo2)
+print(chyslo1 / chyslo2)
+print(chyslo1**chyslo2)
---
+++ HW1/vDykyi/task_1.py
@@ -0,0 +1,7 @@
+name = input("What is your name ? ")
+age = input("What old are you ? ")
+your_location = input("Where do you live ? ")
+
+print("Hello " + (name))
+print("Your age is " + (age))
+print("You live in " + (your_location))
