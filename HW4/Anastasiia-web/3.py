9722dbe35334cf849d15108efd1200203e5c4c4c
Anastasiia-web <anastasiyamarket@gmail.com>
2021-12-02 04:04:45 +0200

    Fibonacci numbers

---
+++ HW4/Anastasiia-web/3.py
@@ -0,0 +1,25 @@
+#  Print Fibonacci numbers up to the entered number n, using cycles. 
+# 1.
+a, b, = 0, 1
+
+while b <= 55:
+    print(b)
+    a, b = b, a + b
+
+# 2
+# Use 'Ctrl + C'to stop the loop))
+a, b, = 0, 1
+
+while b > 0:
+    print(b)
+    a, b = b, a + b
+
+
+
+# My draft
+
+# Чи́сла Фибона́ччи — элементы числовой последовательности 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 
+# 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, …, в которой первые два числа равны 0 и 1, 
+# а каждое последующее число равно сумме двух предыдущих чисел.
+
+# https://www.youtube.com/watch?v=MxQ9_pg0TKA
