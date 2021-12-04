bbaf3470f244487109340cc7e87a56af6fa79f2f
Oleksandr Naumchuk <oolexandrovich@gmail.com>
2021-12-04 18:28:02 +0200

    added homework3_2 and homework3_3

---
+++ HW3/MikeYankee09/Homework3_2.py
@@ -0,0 +1,10 @@
+a = str(4125)
+b = (int(a[0])+int(a[1])+int(a[2])+int(a[3]))
+
+print(b)
+
+# a = int(a[::-1])
+# print(a)
+
+#a = sorted(a)
+#print(sorted(a))
---
+++ HW3/MikeYankee09/Homework3_3
@@ -0,0 +1,5 @@
+x = input("Введіть значення")
+y = input("Введіть значення")
+x, y = y, x
+print(x)
+print (y)
