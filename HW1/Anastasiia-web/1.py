598ccd55ee0094e1fd67c9a51047baa00597a7a3
Anastasiia <61120849+Anastasiia-web@users.noreply.github.com>
2021-12-04 21:07:33 +0200

    Update 1.py

---
+++ HW1/Anastasiia-web/1.py
@@ -0,0 +1,7 @@
+name = input('What is your name?') 
+age = int(input('How old are you?'))  
+country = input('Where do you live?') 
+
+print('Hello', name)
+print('You are', age, 'years old')
+print('You live in great', country!)
