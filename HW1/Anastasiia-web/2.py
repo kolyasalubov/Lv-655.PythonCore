19273c7a173f74260d27b9c83a5dff49569bedf6
Anastasiia <61120849+Anastasiia-web@users.noreply.github.com>
2021-12-04 21:09:58 +0200

    Update 2.py

---
+++ HW1/Anastasiia-web/2.py
@@ -0,0 +1,54 @@
+# Define variables a and b. Read values a and b from console and calculate: 
+# a + b 
+# a - b 
+# a * b 
+# a / b
+# a**b.
+# Output obtained results.
+
+a = 1
+b = 2
+
+a + b 
+print('a + b =', a+b)
+# Output a + b = 3
+
+a - b 
+print('a - b =', a-b)
+# Output a - b = -1
+
+a * b 
+print('a * b =', a*b)
+# Output a * b = 2
+
+a / b
+print('a / b =', a/b)
+# Output a / b = 0.5
+
+a**b
+print('a ** b =', a**b)
+# Output a ** b = 1
+
+c = 3
+d = 2
+print('c + d =', c+d)
+
+print(1/2)
+
+print(10//6)  # how many times can be divided, output 1
+print(10%6) #left after division
+print(2**3) # Exponent - left operant raised to the power of right
+
+print(4+5)
+
+number = 3 + 4 * 5 ** 2 + 7
+print(number)
+
+number = (3 + 4) * (5 ** 2 + 7)
+print(number)
+
+number = 2**3**2
+print(number)
+
+number = (2**3)**2
+print(number)
