dc64a8fca707205ca1229bd32b343ae73dffeed1
VasylZinkov <vasyl.zinkov@gmail.com>
2021-12-05 22:40:28 +0200

    added codewars 2

---
+++ HW6/VZinkov/codewar_2_yelling.py
@@ -0,0 +1,12 @@
+def filter_words(st):
+    """
+    It took some time, i thought that i can't add few commands in one line
+    also i have forgotten about join
+    anyway it worked))
+    """
+    st=st.capitalize()
+    st=st.strip()
+    st=st.replace("    ", " ")
+    st=st.replace("  ", " ")
+    return st
+
