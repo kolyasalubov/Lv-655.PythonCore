0829d70411729f598302d287a7555dc0231f778a
VasylZinkov <vasyl.zinkov@gmail.com>
2021-12-05 21:05:58 +0200

    added HW6 second time

---
+++ HW6/VZinkov/HW_6_1.py
@@ -0,0 +1,15 @@
+def max_1(arg1, arg2):
+    """
+    A function that returns the largest number of two numbers 
+
+    Parameters arg1 & arg2 which should be compared
+
+    Returns: the largest number
+    """
+    if arg1 > arg2:
+        return arg1
+    else:
+        return arg2
+
+
+print(max_1(14, 12.5))
