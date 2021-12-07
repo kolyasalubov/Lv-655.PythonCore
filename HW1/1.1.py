0a53ab311aea045957e48d4a8fb312c23752ddcf
Volodya Mykolyshyn <mykolyshynw@gmail.com>
2021-12-07 17:17:24 +0200

    HW 1,2,3,4,5

---
+++ HW1/1.1.py
@@ -0,0 +1,7 @@
+client_name = input("What is your name?\n")
+client_age = input("How old are you\n")
+client_city = input("Where o you live?\n")
+
+print("Hello, " + client_name)
+print("Your age is " + client_age)
+print("You live in " + client_city)
