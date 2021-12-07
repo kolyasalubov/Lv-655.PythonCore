50f9c899d4601c812972f95de8939e9cc04e477f
Anastasiia-web <anastasiyamarket@gmail.com>
2021-12-07 02:04:58 +0200

    Done

--- HW6/Anastasiia-web/distance.py
+++ HW6/Anastasiia-web/distance.py
@@ -3,6 +3,14 @@
 
 def distance(x1, y1, x2, y2):
     result = round(((((x2-x1)**2)+((y2-y1)**2))**0.5), 2)
+    return result
+
+distance(1, 1, 0, 0)
+
+# Option with printing the result
+
+def distance(x1, y1, x2, y2):
+    result = round(((((x2-x1)**2)+((y2-y1)**2))**0.5), 2)
     print("distance between",(x1,y2),"and",(x1,y2),"is: ", result)
 
 distance(1, 1, 0, 0)
