219f5e613b465dbcf30d38308708d9525c701ba2
Nazar Ravlyk <ravlyk.n@gmail.com>
2021-12-11 17:43:31 +0200

    password

--- HW7/NRavlyk/login.py
+++ HW7/NRavlyk/login.py
@@ -13,10 +13,14 @@ while not good_pass:
     pas_big_letters=len(re.findall(r'[A-Z]',user_pass))
     pas_numbers  = len(re.findall(r'\d',user_pass))
     pas_sym = len(re.findall(r'[$#@]',user_pass))
+    other_sym = len(re.findall(r'[^A-Za-z\d$#@]',user_pass))
+    
 
     if (pas_small_letters and 
         pas_big_letters == 1 and 
-        pas_numbers and pas_sym and 
+        pas_numbers and 
+        pas_sym and 
+        not other_sym and
         6<pas_small_letters+pas_big_letters+pas_numbers+pas_sym<16):
         
         good_pass=True
