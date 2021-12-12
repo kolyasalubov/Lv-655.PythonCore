11dda5b4cc2f8ff84a75d813fc476f1b7d278dba
Nataliia Kovalyk <nataliia.tkachyk@gmail.com>
2021-12-12 15:31:41 +0200

    HW678codewars

---
+++ HW6/nataliia/codewars.py
@@ -0,0 +1,50 @@
+##1 Simple: Find The Distance Between Two Points
+
+# import math
+# def distance(x1, y1, x2, y2):
+#     return round(math.sqrt((x2 - x1)**2 + (y2 - y1)**2),2)
+
+
+##2 No yelling!
+
+# def filter_words(st):
+#     st = st.capitalize()
+#     return ' '.join(st.split())
+
+
+##3 Unfinished Loop - Bug Fixing #1
+
+# def create_array(n):
+#     i = 1
+#     res = []
+#     while i<=n:
+#         res+=[i]
+#         i = i + 1
+#     return res
+
+
+##4 Convert a Number to a String!
+
+# def number_to_string(num):
+#     return (str(num))
+            
+# number_to_string(45)
+
+
+
+##5 Reversing Words in a String
+
+# def reverse(st):
+#     str = st.split()
+#     return ' '.join(reversed(str))
+
+# print(reverse("World Hello"))
+
+
+
+##6 Jenny's secret message
+
+# def greet(name):
+#     if name == "Johnny":
+#         return ("Hello, my love!")
+#     return (f"Hello, {name}!".format(name=name))
