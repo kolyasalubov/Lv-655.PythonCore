0b85249ac74787c9d9a8caa1158d99fb8dc0c9ae
Ihor Dubovskyi <igordsnsu@.gmail.com>
2021-12-13 01:09:56 +0200

    HW6

---
+++ HW6/DubovskyiIhor/Task1.py
@@ -0,0 +1,14 @@
+a = int(input("Input number #1 "))
+b = int(input("Input number #2 "))
+
+def the_largest ():
+    """
+    Define the largest 
+    of two numbers
+    """
+    if a > b:
+        return (a)
+    else:
+        return (b)
+
+print(the_largest()) 
