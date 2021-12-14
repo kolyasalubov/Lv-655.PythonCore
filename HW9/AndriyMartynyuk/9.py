581b4a623c3b253ca5c4a592036668b454c7dad4
Andriy Martynyuk <andriy.martynyuk.if@gmail.com>
2021-12-14 12:34:26 +0200

    10 HW

--- HW9/AndriyMartynyuk/9.py
+++ HW9/AndriyMartynyuk/9.py
@@ -2,25 +2,25 @@ from random import randint
 import re
 # Task 1 Script Game (random number)
 
-# def guess_number():
-#     """
-#     Function to guess a number
-#     """
-#     rand_number = randint(1,100)
-#     user_number = 0
-#     while  user_number != rand_number:
-#         user_number = int(input("Please chose a number: "))
-#         if user_number> rand_number:
-#             print("You number bigger than a randon, try again")
-#             user_number = user_number
-#         elif user_number < rand_number:
-#             print("You number smaller than a randon, try again")
-#             user_number = user_number
-#         else:
-#             print(f'You won, random number is {rand_number}')
-#     return rand_number
+def guess_number():
+    """
+    Function to guess a number
+    """
+    rand_number = randint(1,100)
+    user_number = 0
+    while  user_number != rand_number:
+        user_number = int(input("Please chose a number: "))
+        if user_number> rand_number:
+            print("You number bigger than a randon, try again")
+            user_number = user_number
+        elif user_number < rand_number:
+            print("You number smaller than a randon, try again")
+            user_number = user_number
+        else:
+            print(f'You won, random number is {rand_number}')
+    return rand_number
 
-# guess_number()
+guess_number()
 
 # Task 2 Validity of a password
 def check_user_password():
