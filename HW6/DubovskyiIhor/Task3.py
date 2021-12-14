452c5571ca8d79aa57648e9bb2ac6b351972f743
Ihor Dubovskyi <igordsnsu@.gmail.com>
2021-12-14 05:13:35 +0200

    HW6

--- HW6/DubovskyiIhor/Task3.py
+++ HW6/DubovskyiIhor/Task3.py
@@ -10,23 +10,4 @@ print(word())
 
 
 
-# # def word(**kwargs):
-# #     kwargs=list(a)
-# #     d={}
-# #     for kwarg1 in kwargs:
-# #         kwarg2=kwargs.count(kwarg1)
-# #         d.update({kwarg1:kwarg2})
-# #     return kwargs 
-
-# # print(word())    
-
-
-# 
  
-
-# def word():
-#     for litera in a:
-#     amount=a.count(litera)
-#     print({litera:amount})
-
-# print(word()) 
