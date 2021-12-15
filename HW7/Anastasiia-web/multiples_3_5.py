cb3be1006a0239968137dc6394f8bdcc3a9a716f
Anastasiia-web <anastasiyamarket@gmail.com>
2021-12-15 20:57:01 +0200

    Done

--- HW7/Anastasiia-web/multiples_3_5.py
+++ HW7/Anastasiia-web/multiples_3_5.py
@@ -4,15 +4,14 @@
 # Additionally, if the number is negative, return 0 (for languages that do have them).
 # Note: If the number is a multiple of both 3 and 5, only count it once.
 
-def solution(number):
-    result = []
-    for i in range(0, number):
-        if i % 3 == 0 or i % 5 == 0:
-            result.append(i)
-    return sum(result)
+# def solution(number):
+#     result = []
+#     for i in range(0, number):
+#         if i % 3 == 0 or i % 5 == 0:
+#             result.append(i)
+#     return sum(result)
 
-print(solution(4))
-
+# print(solution(4))
 
 # Notes
 
