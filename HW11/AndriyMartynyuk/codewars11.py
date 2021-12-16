50b376cd39a10a9faf4ed559b37b2af1cd71f327
Andriy Martynyuk <andriy.martynyuk.if@gmail.com>
2021-12-16 09:58:18 +0200

    11 HW

---
+++ HW11/AndriyMartynyuk/codewars11.py
@@ -0,0 +1,57 @@
+# Task 1 Regular Ball Super Ball
+
+# class Ball(object):
+#     def __init__(self, ball_type = "regular"):
+#         self.ball_type = ball_type
+        
+# ball1 = Ball()
+# ball2 = Ball("super")
+
+
+# Task 2 Color Ghost
+
+# import random
+# class Ghost(object):
+#     def __init__(self, color_list = ["white", "yellow", "purple", "red"]):
+#         self.color_list = color_list
+    
+#     def color(self):
+#         self.color_choice = random.choice(self.color_list)
+#         print(self.color_choice)
+        
+# ghost = Ghost()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+# ghost.color()
+
+
+# Task 4 Classy Classes
+
+# class Person:
+#     def __init__(self, name : str, age : int):
+#         self.name = name
+#         self.age = age
+#         self.info = f"{self.name}s age is {self.age}"
+    
+#     def getInfo(self):
+#         return print(self.info)
+
+# human = Person("Andriy", 15)
+# human.getInfo()
