37ba74f2eff0d66f241cfe9e4fbed3c031efeb8c
madydolly <58760412+madydolly@users.noreply.github.com>
2021-12-17 17:15:59 +0200

    V.13.45

--- HW6/Vadym/7.py
+++ HW6/Vadym/7.py
@@ -1,3 +1,15 @@
 # Write a function that returns the largest number of two numbers 
 # (use DocStrings documentation strings in the function). 
 
+def largest_of_two_numbers(first_number, second_number):
+  """ Returns the largest number of two numbers"""
+  if first_number > second_number:
+    return first_number
+  elif first_number==second_number:
+    print ("Numbers are equal") 
+  else:
+    return second_number
+    
+
+a = largest_of_two_numbers(4,3)
+print(a)
