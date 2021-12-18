656bc73e34573914c9c6c1adb77fce8467aa824a
Anastasiia <61120849+Anastasiia-web@users.noreply.github.com>
2021-12-18 19:14:53 +0200

    Update 1.py

--- HW8/Anastasiia-web/1.py
+++ HW8/Anastasiia-web/1.py
@@ -24,3 +24,27 @@ r = Rectangle()
 r.inputSides()
 r.dispSides()
 r.findArea()
+
+# Option with user input separated by coma
+import re
+
+class Polygon:
+    def __init__(self, sides_a_b):
+        self.sides = sides_a_b
+
+    def inputSides(self):       
+        sides_a_b = input('Enter 2 numbers separated by coma: ')
+        nums = re.findall(r'\d+', sides_a_b)   
+        self.sides = [int(i) for i in nums]  
+
+class Rectangle(Polygon):
+    def __init__(self):
+        super().__init__(self)  
+    def findArea(self):
+        a, b = self.sides
+        area = a*b
+        print(f'The area of the rectangle is {area}')
+
+r = Rectangle()
+r.inputSides()
+r.findArea()
