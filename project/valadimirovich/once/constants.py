2ac6823ff2fdcdc1ee3d1663ecb42c6a8dfac013
Dima Evpak <valadimirovich@gmail.com>
2021-12-22 15:49:32 +0200

    Added requirements

--- project/valadimirovich/once/constants.py
+++ project/valadimirovich/once/constants.py
@@ -1,1 +1,2 @@
-url_access_token = "https://api.1nce.com/management-api/oauth/token"
+url_access_token = 'https://api.1nce.com/management-api/oauth/token'
+sms_get_post_url = 'https://api.1nce.com/management-api/v1/sims/{iccid}/sms'
