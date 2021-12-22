3fcefb5194ef1f04c9e3a5626dc18f6e01ecf2ff
Dima Evpak <valadimirovich@gmail.com>
2021-12-23 00:21:25 +0200

    small corrections

--- HW7/valadimirovich/Wome_Work7_Task_2.py
+++ HW7/valadimirovich/Wome_Work7_Task_2.py
@@ -6,17 +6,18 @@ invalid = True
 
 while invalid:
     
-    if re.findall(r'[a-z]', user_pass) != [] \
-        and re.findall(r'[A-Z]', user_pass)!=[] \
-        and re.findall("#|@|$", user_pass)!=[] \
-        and re.findall(r'[0-9]', user_pass)!=[] \
-        and 6 < len(user_pass) < 16:
-        invalid = False
+    # if re.findall(r'[a-z]', user_pass) != [] \
+    #     and re.findall(r'[A-Z]', user_pass)!=[] \
+    #     and re.findall("#|@|$", user_pass)!=[] \
+    #     and re.findall(r'[0-9]', user_pass)!=[] \
+    #     and 6 < len(user_pass) < 16:
+    #     invalid = False
 
     # # And it could be as simple as that:
-    # user_pass = input("Please create your pasword: ")
-    # if re.findall("^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[@#$])[a-zA-Z/\d@#$]{6,16}$", user_pass):
-    # invalid = False
+    user_pass = input("Please create your pasword: ")
+    
+    if re.findall("(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[@#$])[a-zA-Z/\d@#$]{6,16}", user_pass):
+            invalid = False
     
 
 print('You did it, you son of a gun!')
