3dd507e744abbcd500ce5195227e6c8f7536ac02
Dima Evpak <valadimirovich@gmail.com>
2021-12-23 00:17:58 +0200

    Now i clearified reEx for myself

--- HW7/valadimirovich/Wome_Work7_Task_2.py
+++ HW7/valadimirovich/Wome_Work7_Task_2.py
@@ -6,14 +6,17 @@ invalid = True
 
 while invalid:
     
-    user_pass = input("Please create your pasword: ")
-
     if re.findall(r'[a-z]', user_pass) != [] \
         and re.findall(r'[A-Z]', user_pass)!=[] \
         and re.findall("#|@|$", user_pass)!=[] \
         and re.findall(r'[0-9]', user_pass)!=[] \
         and 6 < len(user_pass) < 16:
         invalid = False
+
+    # # And it could be as simple as that:
+    # user_pass = input("Please create your pasword: ")
+    # if re.findall("^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[@#$])[a-zA-Z/\d@#$]{6,16}$", user_pass):
+    # invalid = False
     
 
 print('You did it, you son of a gun!')
