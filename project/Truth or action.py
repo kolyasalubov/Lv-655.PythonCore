54b32ee90f2826f3086501fbddea48ac81f811d7
Taras Marysiak <marusiak.tar@gmail.com>
2021-12-23 01:27:19 +0300

    project

---
+++ project/Truth or action.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from random import*
+import questions
+
+# створюємо вікно
+root = Tk()
+
+root.geometry('800x450')#задаємо розмір вікна
+root.title("Truth or action")# задаємо назву вікна
+root.wm_attributes('-alpha', 0.9)#додали прозорісь вікна
+root.resizable(width=False, height=False)#заборагили змінювати розмір вікна
+
+image_skr = PhotoImage(file = 'E:123.png')# встановлюмо фон основного вікна
+skrin = Label(root, image = image_skr)
+skrin.grid()
+
+count =0
+count_ques=len(questions.ques)
+count_task=len(questions.task)
+
+# логіка роботи блоку "Правда"
+def quest():
+    global count
+    global count_ques
+    lbl_result["text"] = element = choice(questions.ques)# функція рандому для запитань "Правда"
+    questions.ques.remove(element)#видаляємо елемент який випав для уникнення повторів
+    count+=1
+    if count==count_ques:#умова друку повідомлення про закінчення гри
+        lbl_result["foreground"] ="Green"
+        lbl_result["text"] ="Гра закінчена.\n Ви відповіли на всі питання блоку 'Правда'"
+        
+# логіка роботи блоку "Дія"
+def quest1():
+    global count
+    global count_task
+    element= choice(questions.task)# функція рандому для завдань "Дія" 
+    lbl_result["text"] = element
+    questions.task.remove(element)#видаляємо елемент який випав для уникнення повторів
+    count+=1
+    if count==count_task:#умова друку повідомлення про закінчення гри
+        lbl_result["foreground"] ="Green"
+        lbl_result["text"] ="Гра закінчена.\n Ви виконали всі завдання блоку 'Дія'"
+  
+# створюємо кнопку "Правда"
+btn = Button(root,
+             text="Правда",
+             width=15,
+             height=2, 
+             bg="green",
+             font="Verdana 20 italic",
+             command=quest)
+btn.place(relx=0.1, rely=0.75) #Задаєм координати розміщення кнопки
+
+# створюємо кнопку "дія"
+btn1 = Button(root, 
+              text="Дія",
+              width=15,
+              height=2, 
+              bg="red",
+              font="Verdana 20 italic",
+              command=quest1)       
+btn1.place(relx=0.56, rely=0.75)
+
+# загаловок та текст завань
+lbl_result = Label(text="Обирай сказати правду чи виконати завдання",
+                   foreground="red",
+                    background="White",
+                    font='arial 25',
+                    )
+lbl_result.place(relx=.5, rely=.3, anchor="c", bordermode=OUTSIDE)
+
+mainloop()
