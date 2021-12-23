90b0acf4d7ec2929b7301908726105d33c221295
AlinaSiriachenko <alinasiriachenko@gmail.com>
2021-12-23 17:10:29 +0200

    Dragon Arena Game Update 1.0

--- Dragon_Arena_Game/battle.py
+++ Dragon_Arena_Game/battle.py
@@ -4,15 +4,19 @@ from pygame import mixer
 
 pygame.init()
 
-#set framerate
+#set framerate. or else animations will run with different speed depending on a
+# computer's speed
+# this function is used to create a clock object which can be used to keep 
+# track of time.
 clock = pygame.time.Clock()
 fps = 60
 
-#game_window
+#game window size
 bottom_panel = 200
 screen_width = 1280
 screen_height = 1024 + bottom_panel
 
+#create game window
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Dragon Arena")
 
@@ -22,7 +26,6 @@ total_fighters = 5
 action_cooldown = 0
 action_wait_time = 90
 attack = False
-potion = False
 clicked = False
 
 
@@ -30,18 +33,24 @@ clicked = False
 font = pygame.font.SysFont('Times New Roman', 36)
 
 #define font colors
-red = (255, 0, 0)
+red = (139, 0, 0)
 green = (0, 255, 0)
 
 
-#load images and music
-# background image
+# load images and music. loads all of your assets/music into memory
+# This is outside of the 'while' loop or else the loading will be happening
+# each iteration. 
+# So I load all my assets upfront
+# 'convert_alpha()' - converts the asset and maintains the alpha channel
+# (transparency)
+# load background image
 background_img = pygame.image.load('Dragon_Arena_Game/img/Background/background.jpg').convert_alpha()
-# background sound
+# load background sound
 mixer.music.load('Dragon_Arena_Game/sound/background.wav')
-mixer.music.play(-1)
-# panel image
+mixer.music.play(-1)  # (-1) to cycle the music
+# load panel image
 panel_img = pygame.image.load('Dragon_Arena_Game/img/Icons/panel.jpg').convert_alpha()
+# load sword mouse icon
 sword_img = pygame.image.load('Dragon_Arena_Game/img/Icons/sword.png').convert_alpha()
 
 
@@ -51,43 +60,44 @@ def draw_text(text, font, text_col, x, y):
     screen.blit(img, (x, y))
 
 
-#function for drawing background
+# function for drawing background 
+# I put it onto window 'screen' 
+# 'blit' - function that puts the image onto the screen
+# I give it the name of the image - 'background_img'
+# (0, 0) - coordinats, puts it in the top left corner of the screen
 def draw_bg():
     screen.blit(background_img, (0, 0))
+
 
 #function for drawing panel
 def draw_panel():
     #draw panel rectangle
     screen.blit(panel_img, (0, screen_height - bottom_panel))
-    #show dragon stats
-    # draw_text(f'{golden_dragon.name} HP: {golden_dragon.hp}', font, red, 100, screen_height - bottom_panel + 20)
+    #show all the creatures stats
     for count, i in enumerate(dragon_list):
-        #show name and health
+        #show name and health of dragons
         draw_text(f'{i.name} HP: {i.hp}', font, green, 100, (screen_height - bottom_panel + 20) + count * 60)
     for count, i in enumerate(monster_list):
-        #show name and health
+        #show name and health of monsters
         draw_text(f'{i.name} HP: {i.hp}', font, red, 750, (screen_height - bottom_panel + 20) + count * 60)
 
 
 # Class for all creatures
 class Fighter():
-    def __init__(self, x, y, name, max_hp, strength, potions):
+    def __init__(self, x, y, name, max_hp, strength):
         self.name = name
         self.max_hp = max_hp
         self.hp = max_hp
         self.strength = strength
-        self.start_potions = potions
-        self.potions = potions
+        # each time I create an instance of the class, it starts off with
+        # 'alive' being set to 'True'
         self.alive = True
-
         self.animation_list = []
         self.frame_index = 0
-
         self.action = 0 #0: idle, 1:ability_1
-
         self.update_time = pygame.time.get_ticks()
 
-        #load idle images
+        #load idle images of fighters
         temp_list = []
         for i in range(8):
             img = pygame.image.load(f"Dragon_Arena_Game/img/{self.name}/Idle/{i}.png")
@@ -95,7 +105,7 @@ class Fighter():
             temp_list.append(img)
         self.animation_list.append(temp_list)
 
-        #load attack ability_1 images
+        #load attack ability_1 images of fighters
         temp_list = []
         for i in range(8):
             img = pygame.image.load(f"Dragon_Arena_Game/img/{self.name}/Ability_1/{i}.png")
@@ -105,22 +115,27 @@ class Fighter():
 
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
+        # will position an image onto the screen to the coordinats given here
         self.rect.center = (x, y)
 
 
     def update(self):
+        # animation is gonna be based on time, so after certain amount
+        # of time has passed, I want the image to move on to the next
+        # one within the list
         animation_cooldown = 100
-        #handle animation
-        #update image
+        # handle animation, update image
         self.image = self.animation_list[self.action][self.frame_index]
-        #check if enough time has passed since the last update
+        # check if enough time has passed since the last update
         if pygame.time.get_ticks() - self.update_time > animation_cooldown:
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
-        #if the animation has run out, then reset back to the start
+        # if the animation has run out, then reset back to the start
         if self.frame_index >= len(self.animation_list[self.action]):
             self.idle()
 
+
+    # draw fighters images, with position self.rect
     def draw(self):
         screen.blit(self.image, self.rect)
 
@@ -161,18 +176,18 @@ class HealthBar():
         pygame.draw.rect(screen, green, (self.x, self.y, 200 * ratio, 15))
 
 
-golden_dragon = Fighter(200, 260, 'Golden Dragon', 30, 10, 3)
-forest_dragon = Fighter(300, 460, 'Forest Dragon', 30, 10, 3)
-silver_dragon = Fighter(200, 660, 'Silver Dragon', 30, 10, 3)
+golden_dragon = Fighter(200, 260, 'Golden Dragon', 30, 10)
+forest_dragon = Fighter(300, 460, 'Forest Dragon', 30, 10)
+silver_dragon = Fighter(200, 660, 'Silver Dragon', 30, 10)
 
 dragon_list = []
 dragon_list.append(golden_dragon)
 dragon_list.append(forest_dragon)
 dragon_list.append(silver_dragon)
 
-monster_snake_1 = Fighter(1010, 260, 'Monster Snake', 30, 6, 1)
-monster_snake_2 = Fighter(910, 460, 'Monster Snake', 30, 6, 1)
-monster_snake_3 = Fighter(1010, 660, 'Monster Snake', 30, 6, 1)
+monster_snake_1 = Fighter(1010, 260, 'Monster Snake', 30, 6)
+monster_snake_2 = Fighter(910, 460, 'Monster Snake', 30, 6)
+monster_snake_3 = Fighter(1010, 660, 'Monster Snake', 30, 6)
 
 monster_list = []
 monster_list.append(monster_snake_1)
@@ -188,16 +203,17 @@ monster_snake_2_health_bar = HealthBar(750, screen_height - bottom_panel + 120, 
 monster_snake_3_health_bar = HealthBar(750, screen_height - bottom_panel + 180, monster_snake_3.hp, monster_snake_3.max_hp)
 
 
-
+#while loop with a True condition is for the game to run constantly
 run = True
 while run:
 
+    # sets fps to 60
     clock.tick(fps)
 
-    #draw background
+    # draw background
     draw_bg()
 
-    #draw panel
+    # draw panel and hp bars
     draw_panel()
     golden_dragon_health_bar.draw(golden_dragon.hp)
     forest_dragon_health_bar.draw(forest_dragon.hp)
@@ -206,36 +222,29 @@ while run:
     monster_snake_2_health_bar.draw(monster_snake_2.hp)
     monster_snake_3_health_bar.draw(monster_snake_3.hp)
 
-    #draw fighters
+    # draw fighters
     for dragon in dragon_list:
         dragon.update()
         dragon.draw()
 
-    # golden_dragon.update()
-    # golden_dragon.draw()
-    # forest_dragon.update()
-    # forest_dragon.draw()
-    # silver_dragon.update()
-    # silver_dragon.draw()
     for monster in monster_list:
         monster.update()
         monster.draw()
 
 
-
-    #control player actions
-    #reset action variables
+    # mouse
+    # control player actions
+    # reset action variables
     attack = False
-    potion = False
     target = None
-    #make sure mouse is visible
+    # make sure mouse is visible
     pygame.mouse.set_visible(True)
     pos = pygame.mouse.get_pos()
     for count, monster in enumerate(monster_list):
         if monster.rect.collidepoint(pos):
-            #hide mouse
+            # hide mouse
             pygame.mouse.set_visible(False)
-            #show sword in place of mouse cursor
+            # show sword in place of mouse cursor
             screen.blit(sword_img, pos)
             if clicked == True:
                 attack = True
@@ -244,13 +253,13 @@ while run:
 
 
 
-    #player action
+    # player action
     for count, dragon in enumerate(dragon_list):
         if current_fighter == 0 + count:
             if dragon.alive == True:
                 action_cooldown += 1
                 if action_cooldown >= action_wait_time:
-                    #attack
+                    # look for player action: attack
                     if attack == True and target != None:
                         dragon.attack(target)
                         current_fighter += 1
@@ -259,24 +268,31 @@ while run:
                 current_fighter += 1
 
 
-    #enemy action
+    # enemy action
     for count, monster in enumerate(monster_list):
         if current_fighter == 3 + count:
             if monster.alive == True:
                 action_cooldown += 1
                 if action_cooldown >= action_wait_time:
-                    #attack
+                    # attack
                     monster.attack(random.choice(dragon_list))
                     current_fighter += 1
                     action_cooldown = 0
             else:
                 current_fighter += 1
 
-#if all fighters have had a turn then reset
+# if all fighters have had a turn then reset
     if current_fighter > total_fighters:
         current_fighter = 0
 
 
+# Because of the while=True, the game will run forever
+# If I run the while again, I will get stuck with it with no way of exiting
+# or control
+# So what I need is to be able to loop for all events/interactions
+# that could possibly occur
+# I will use PyGame event handler 'for' loop
+# it looks for mouse clicks, button presses, etc.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -285,6 +301,9 @@ while run:
         else:
             clicked = False
 
+
     pygame.display.update()
 
+
+# exits pygame
 pygame.quit()
